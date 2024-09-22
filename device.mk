f67f7d4d894f9064a0dd6e12a841dd73ec7b4395
bengris32 <bengris32@protonmail.ch>
2024-09-22 23:04:46 +0000

    Vitamin: Ship AOSP cgroups and task_profiles configs

--- device.mk
+++ device.mk
@@ -137,6 +137,10 @@ PRODUCT_PACKAGES += \
 # Characteristics
 PRODUCT_CHARACTERISTICS := nosdcard
 
+# Cgroup
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json 
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
